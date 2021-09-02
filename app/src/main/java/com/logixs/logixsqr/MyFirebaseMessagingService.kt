a6fdf10ae985d7f047dba3407d288cc18c3c9521
Horacio <haalvarez1974@gmail.com>
2021-09-02 10:31:03 -0300

    mensajeria funcionando v2 webview reload

--- app/src/main/java/com/logixs/logixsqr/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/logixs/logixsqr/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package com.logixs.logixsqr
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.logixs.logixsqr.ui.WebView.WebViewFragment
@@ -10,20 +11,33 @@ import com.logixs.logixsqr.ui.WebView.WebViewFragment
 
 class MyFirebaseMessagingService:FirebaseMessagingService() {
 
-    override fun onMessageReceived(p0: RemoteMessage) {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         abrir()
-        val noti=p0.getNotification()
-        val data=p0.getData()
+        val noti=remoteMessage.getNotification()
+        val data: Map<String, String> = remoteMessage.getData()
        Looper.prepare()
         Handler().post{
 
-            Toast.makeText(baseContext,p0.notification?.title,Toast.LENGTH_LONG).show()
+            Toast.makeText(baseContext,remoteMessage.notification?.title,Toast.LENGTH_LONG).show()
         }
-
-
-
-
         Looper.loop()
+        val myCustomKey = data["dato1"]
+
+
+        var notification = NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ic_menu_camera)
+            .setContentTitle("setContentTitle")
+            .setContentText(myCustomKey)
+            //.setLargeIcon(R.drawable.circular)
+            .setStyle(NotificationCompat.BigTextStyle())
+                //.bigText(emailObject.getSubjectAndSnippet()))
+            .build()
+
+
+
+
+
+
 
        //}
 
